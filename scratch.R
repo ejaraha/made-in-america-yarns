57db0083423b9b155e33356ca33322c12e9a89ac
Ess Jaraha <37634991+sjaraha@users.noreply.github.com>
2021-03-23 16:23:09 -0400

    select, load only necessary fields

--- scratch.R
+++ scratch.R
@@ -1,8 +1,25 @@
 library(dplyr)
 
+# set working directory
 setwd("C:/Users/sjara/git/made-in-america-yarns/data/raw")
-coupon <- read.csv("coupon.csv")
-order <- read.csv("order.csv")
-product <- read.csv("product.csv")
 
-glimpse(coupon)
+
+# load keys to select necessary fields
+coupon_keep <- read.csv("coupon_keep_fields.csv") %>%
+  filter(keep==1)
+order_keep <- read.csv("order_keep_fields.csv") %>%
+  filter(keep==1)
+product_keep <- read.csv("product_keep_fields.csv") %>%
+  filter(keep==1)
+
+# load raw data then select necessary fields
+coupon <- read.csv("coupon.csv") %>%
+  select(coupon_keep$variable)
+order <- read.csv("order.csv") %>%
+  select(order_keep$variable)
+product <- read.csv("product.csv") %>%
+  select(product_keep$variable)
+
+glimpse(order)
+glimpse(product_keep)
+
