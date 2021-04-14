032e24fb47d437c99ac21d98224143831c2e8055
Ess Jaraha <37634991+sjaraha@users.noreply.github.com>
2021-04-14 12:07:40 -0400

    change necessary  fields

--- scratch.R
+++ scratch.R
@@ -3,23 +3,17 @@ library(dplyr)
 # set working directory
 setwd("C:/Users/sjara/git/made-in-america-yarns/data/raw")
 
-
 # load keys to select necessary fields
-coupon_keep <- read.csv("coupon_keep_fields.csv") %>%
+order_fields <- read.csv("order_fields.csv") %>%
   filter(keep==1)
-order_keep <- read.csv("order_keep_fields.csv") %>%
-  filter(keep==1)
-product_keep <- read.csv("product_keep_fields.csv") %>%
+product_fields <- read.csv("product_fields.csv") %>%
   filter(keep==1)
 
 # load raw data then select necessary fields
-coupon <- read.csv("coupon.csv") %>%
-  select(coupon_keep$variable)
 order <- read.csv("order.csv") %>%
-  select(order_keep$variable)
+  select(order_fields$variable)
 product <- read.csv("product.csv") %>%
-  select(product_keep$variable)
+  select(product_fields$variable)
 
-glimpse(order)
-glimpse(product_keep)
+
 
