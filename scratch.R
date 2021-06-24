d64a51b7ccd4ced17ee0f52a4ee734628d50a183
Ess Jaraha <37634991+sjaraha@users.noreply.github.com>
2021-06-24 13:56:04 -0400

    routine update

--- scratch.R
+++ scratch.R
@@ -1,5 +1,13 @@
-data$product_hue %>% 
-  mutate(across(.cols= everything(), as.character)) %>%
-  filter(if_any(.cols= contains("hue"), ~.%in% c("blue", "yellow"))) %>%
+ga <- read.csv("C:/Users/sjara/Downloads/ga_april.csv", stringsAsFactors = FALSE)
+wp <- read.csv("C:/Users/sjara/Downloads/wp_april.csv", stringsAsFactors = FALSE) %>% 
+  select(-contains(c("line_item", "billing")))
+
+wp_only <- wp %>% 
+  left_join(ga, by=c("order_id"="Transaction.ID")) %>% 
+  filter(is.na(Date)==TRUE) %>%
   glimpse()
 
+both <- wp %>% 
+  left_join(ga, by=c("order_id"="Transaction.ID")) %>% 
+  filter(is.na(Date)==FALSE) %>%
+  glimpse()
